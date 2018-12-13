2c8f6b15fb96ef188b0726cc2ab402a285641329
Viet Asc <vtahdn@gmail.com>
2018-12-13 10:13:19 +0700

    Initial Commit

---
+++ SQLiteMusic_ViewBase_v3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SQLiteMusic_ViewBase_v3
+//
+//  Created by Viet Asc on 12/13/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SQLiteMusic_ViewBase_v3Tests/SQLiteMusic_ViewBase_v3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SQLiteMusic_ViewBase_v3Tests.swift
+//  SQLiteMusic_ViewBase_v3Tests
+//
+//  Created by Viet Asc on 12/13/18.
+//  Copyright © 2018 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import SQLiteMusic_ViewBase_v3
+
+class SQLiteMusic_ViewBase_v3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
